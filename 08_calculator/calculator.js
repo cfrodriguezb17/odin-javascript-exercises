27ba9b32fc37bd762c4a1f36daac2cf9d605edee
Carlos Rodriguez <cfrodriguezb17@gmail.com>
2022-08-07 09:03:18 -0500

    Calculator finished

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function(a, b) {
+	let res = a + b;
+  return res;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	let res = a - b;
+  return res;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let res = 0;
+  if(array.length > 0){
+    for(const num of array){
+      res += num;
+    }
+  }
+  return res;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let res = 0;
+  if(array.length > 0){
+    res = array[0];
+    for(let i = 1;i < array.length; i++){
+      res *= array[i];
+    }
+  }
+  return res;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let res = a ** b;
+  return res;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let res = 1;
+  if(num > 1){
+    for(let i = 1; i < num + 1; i++){
+      res *= i;
+    }
+  }
+  return res;
 };
 
 // Do not edit below this line
