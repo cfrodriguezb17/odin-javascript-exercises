52497c4e3857a801a8f56c83253d77c872568f4f
Carlos Rodriguez <cfrodriguezb17@gmail.com>
2022-08-07 12:15:41 -0500

    Finish fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  if(num < 0) return 'OOPS'
+  if(typeof num === 'string') num = Number(num)
+  if(num == 1) return 1
+  let num0 = 0;
+  let num1 = 1;
+  let res;
+  if(num > 0){
+    for(let i = 0; i < num - 1; i++){
+      res = num0 + num1;
+      num0 = num1;
+      num1 = res;
+    }
+  }
+  return res;
 };
 
 // Do not edit below this line
