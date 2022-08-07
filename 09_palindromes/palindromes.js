7ba3083ff57f2f23640f44422ae6e77951bea3a3
Carlos Rodriguez <cfrodriguezb17@gmail.com>
2022-08-07 11:25:58 -0500

    Finish palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+let palindromes = function (quote) {
+  let invertQuote = '';
+  quote = quote.toLowerCase();
+  // creo que hace doble comprobacion
+  quote = quote.replaceAll(/\s/g,'');
+  quote = quote.replace(/[.,/#!$%^&*;:{}=-_`~()]/g, "").replace(/s{2,}/g, " ");
+  for(let i = quote.length;i  >= 0; i--){
+    invertQuote = invertQuote + quote.charAt(i);
+  }
+  if (quote == invertQuote) {
+    return true
+  }else{
+    return false
+  }
 };
 
 // Do not edit below this line
